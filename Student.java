33abc371c949dcd1bd81ce65d342e62492f35829
prikshitgaur772@gmail.com <prikshitgaur772@gmail.com>
2025-04-18 20:04:28 +0530

    Student

---
+++ Student.java
@@ -0,0 +1,25 @@
+public class Student {
+    private int rollNo;
+    private String name;
+    private int age;
+
+    // Constructor
+    public Student(int rollNo, String name, int age) {
+        this.rollNo = rollNo;
+        this.name = name;
+        this.age = age;
+    }
+
+    // Getter and Setter methods
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
